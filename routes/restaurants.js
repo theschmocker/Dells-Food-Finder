4fe4942edd09973bd70da7bb91d906f77c218cbb
theschmocker <jacobcs1996@gmail.com>
2018-05-08 09:50:19 -0500

    add basic route to send list of restaurants

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const getRestaurants = require('../util/getGPlaces');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json(require('../restaurants.json'));
+router.get('/', async (req, res, next) => {
+    const restaurants = await getRestaurants();
+    if (!restaurants.ok) {
+        res.locals.error = restaurants;
+        return next();
+    }
+
+    return res.json(restaurants);
+});
+
+router.use('/', async (req, res) => {
+    res.json(res.locals);
 });
 
 module.exports = router;
