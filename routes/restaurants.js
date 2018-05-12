2b2d2560ef37b6810c5375c463f2c950abc149d6
theschmocker <jacobcs1996@gmail.com>
2018-05-11 22:13:33 -0500

    query the db instead of direct Places API call. add some comments

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const getRestaurants = require('../util/getGPlaces');
+
+const Restaurant = require('../models/Restaurant');
 
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
-    const restaurants = await getRestaurants();
+    // Query db for all the restaurants
+    const restaurants = await Restaurant.all();
+
+    // If something went wrong with the query, pass it along to error middleware
     if (!restaurants.ok) {
         res.locals.error = restaurants;
         return next();
     }
-
+    
     return res.json(restaurants);
 });
 
+// error middleware
 router.use('/', async (req, res) => {
-    res.json(res.locals);
+    return res.json(res.locals);
 });
 
 module.exports = router;
