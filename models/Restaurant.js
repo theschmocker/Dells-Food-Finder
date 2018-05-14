c113368434a84038b70bf6c141c43d07003a3365
theschmocker <jacobcs1996@gmail.com>
2018-05-14 10:37:04 -0500

    restaurant query includes the number of votes for each restaurant

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,4 +1,4 @@
-const db = require('../db');
+const knex = require('../db');
 
 
 (async () => {
@@ -6,7 +6,10 @@ const db = require('../db');
 
 async function all() {
     try {
-        const restaurants = await db.select('*').from('restaurants');
+        const restaurants = await knex('restaurants')
+            .select('restaurants.*', knex.raw('COUNT(votes.restaurant_id) AS votes'))
+            .leftJoin('votes', 'restaurants.id', 'votes.restaurant_id')
+            .groupBy('restaurants.id')
         return {
             ok: true,
             data: restaurants,
