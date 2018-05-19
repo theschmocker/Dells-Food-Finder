ddc9919e1ae7f83b52d2a103ab2c49281ed68806
theschmocker <jacobcs1996@gmail.com>
2018-05-18 22:24:36 -0500

    parse opening_hours before returning restaurants

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,15 +1,20 @@
 const knex = require('../db');
 
-
-(async () => {
-})();
-
 async function all() {
     try {
-        const restaurants = await knex('restaurants')
+        const dbResponse = await knex('restaurants')
             .select('restaurants.*', knex.raw('COUNT(votes.restaurant_id) AS votes'))
             .leftJoin('votes', 'restaurants.id', 'votes.restaurant_id')
-            .groupBy('restaurants.id')
+            .groupBy('restaurants.id');
+
+        const restaurants = dbResponse.map(r => {
+            return {
+                ...r,
+                // stored as JSON in db. Parse
+                opening_hours: JSON.parse(r.opening_hours)
+            };
+        });
+
         return {
             ok: true,
             data: restaurants,
