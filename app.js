296acdcc4a942dbd5c60bfccfaabf2dce02d4eb4
theschmocker <jacobcs1996@gmail.com>
2018-05-20 15:32:49 -0500

    serve static files

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const apiRoutes = require('./routes');
 
 const app = express();
 
+app.use(express.static('client/build'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
