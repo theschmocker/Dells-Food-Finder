8fe266d22adb0c3fb2f93239fbcc216ffdc1e5a7
theschmocker <jacobcs1996@gmail.com>
2018-05-26 18:08:19 -0500

    componentize!

--- client/src/components/Randomizer/Randomizer.js
+++ client/src/components/Randomizer/Randomizer.js
@@ -1,61 +1,24 @@
 import React from 'react';
 
 import Restaurant from '../Restaurant';
+import PickerButton from './PickerButton';
+import NotPicked from './NotPicked';
+import PickedRestaurant from './PickedRestaurant';
 
 import RestaurantsContext from '../restaurants-context.js';
 
-function randomRestaurant(restaurants, lastPicked) {
-    const getRandomRestaurant = () => {
-        const randomIndex = Math.floor(Math.random() * restaurants.length + 1);
-        const pickedRestaurant = restaurants[randomIndex];
-
-        return pickedRestaurant;
-    }
-
-    const sameRestaurant = (lastRestaurant, nextRestaurant) => {
-        return lastRestaurant !== undefined && lastRestaurant.id === nextRestaurant.id;
-    }
-
-    // pick restaurant
-    let restaurant = getRandomRestaurant();
-
-    // if restaurant is undefined or the same as the last restaurant, pick a new one
-    while (!restaurant || sameRestaurant(lastPicked, restaurant)) {
-        restaurant = getRandomRestaurant() 
-    }
-
-    return restaurant;
-
-}
+import './styles.css'
 
 const Randomizer = () => (
-    <React.Fragment>
-        <RandomizerButton />
+    <div className="randomizer">
         <RestaurantsContext.Consumer>
             {({ pickedRestaurant }) => {
                 return pickedRestaurant 
-                    ? <Restaurant restaurant={pickedRestaurant}/>
-                    : <p>nothing</p>
+                    ? <PickedRestaurant restaurant={pickedRestaurant} />
+                    : <NotPicked />
             }}
         </RestaurantsContext.Consumer>
-    </React.Fragment>
-)
-
-const RandomizerButton = () => (
-        <RestaurantsContext.Consumer>
-            {({ restaurants, pickedRestaurant, updatePickedRestaurant }) => {
-                return (
-                    <button 
-                        onClick={() => {
-                            const restaurant = randomRestaurant(restaurants, pickedRestaurant);
-                            updatePickedRestaurant(restaurant)
-                        }}
-                    >
-                        Random Restaurant
-                    </button>
-                )
-            }}
-        </RestaurantsContext.Consumer>
+    </div>
 )
 
 export default Randomizer;
