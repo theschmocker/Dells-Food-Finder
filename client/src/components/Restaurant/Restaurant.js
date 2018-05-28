c1b62cad78729ed75da29dc9e9564b2cab9e38ba
theschmocker <jacobcs1996@gmail.com>
2018-05-27 19:50:22 -0500

    add new components

--- client/src/components/Restaurant/Restaurant.js
+++ client/src/components/Restaurant/Restaurant.js
@@ -1,62 +1,32 @@
 import React from 'react';
 
-//{JSON.parse(restaurant.opening_hours)
-//                .weekday_text
-//.map(day => <li>{day}</li>)}
+import OpenStatus from './OpenStatus';
+import AddressLink from './AddressLink';
+import WebsiteLink from './WebsiteLink';
+import Hours from './Hours';
+
 const Restaurant = ({ restaurant }) => {
-    const periods = restaurant && restaurant.opening_hours ? restaurant.opening_hours.periods : null;
     return (
         <div className="restaurant">
             <h2 className="restaurant__name">{restaurant.name}</h2>
-            <p>
-                {!!periods 
-                        ? 
-                            isOpen(periods) 
-                                ? 'Open' 
-                                : 'Closed'
-                        : 
-                            'Unknown'
-                }
-
-            </p>
+            <AddressLink 
+                url={restaurant.url}
+                address={restaurant.formatted_address}
+            />
+            <WebsiteLink 
+                url={restaurant.website}
+                name={restaurant.name}
+            />
+            <Hours 
+                hours={restaurant 
+                        && restaurant.opening_hours 
+                        && restaurant.opening_hours.weekday_text}
+            />
+            <OpenStatus restaurant={restaurant}/>
             <ul className="restaurant__hours">
             </ul>
         </div>
     )
-}
-
-function isOpen(periods) {
-    const today = new Date();
-    const now = today.getTime();
-    const period = periods[today.getDay()];
-
-
-    // open 24 hours
-    if (period.open && !period.close) return true;
-
-    const openMS = getPeriodMilliseconds(period.open, today);
-    const closeMS = getPeriodMilliseconds(period.close, today);
-
-    return (now >= openMS && now < closeMS);
-}
-
-function getPeriodMilliseconds(period, today) {
-    const hours = period.time.slice(0, 2);
-    const minutes = period.time.slice(2, 4);
-    
-    const date = new Date();
-    date.setHours(hours)
-    date.setMinutes(minutes)
-
-    // handle case where restaurant closes at or after midnight
-    if (period.day > today.getDay()) {
-        const difference = period.day - today.getDay();
-        date.setDate(today.getDate() + difference);
-    }
-
-    
-
-    return date.getTime()
 }
 
 export default Restaurant;
