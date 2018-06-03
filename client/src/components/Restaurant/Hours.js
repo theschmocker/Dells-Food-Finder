2a844a905279d7401745ba66294fd90c78e6d3cb
theschmocker <jacobcs1996@gmail.com>
2018-06-03 00:28:11 -0500

    remove wrapping div

--- client/src/components/Restaurant/Hours.js
+++ client/src/components/Restaurant/Hours.js
@@ -2,11 +2,9 @@
 import React from 'react';
 
 const Hours = ({ hours, className }) => (    
-    <div className={className}>
-        <ul>
-            {hours && hours.map(h => <li key={h}>{h}</li>)}
-        </ul>
-    </div>
+    <ul className={className}>
+        {hours && hours.map(h => <li key={h}>{h}</li>)}
+    </ul>
 )
 
 export default Hours;
