911bedc7d419a23aabe323b59bc2c245f4ee2e64
theschmocker <jacobcs1996@gmail.com>
2018-06-04 21:32:44 -0500

    rewrite as standard component. add fake loading method

--- client/src/components/Randomizer/Randomizer.js
+++ client/src/components/Randomizer/Randomizer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Restaurant from '../Restaurant';
 import PickerButton from './PickerButton';
@@ -7,16 +7,30 @@ import PickedRestaurant from './PickedRestaurant';
 
 import RestaurantsContext from '../restaurants-context.js';
 
-const Randomizer = () => (
-    <div className="randomizer">
-        <RestaurantsContext.Consumer>
-            {({ pickedRestaurant }) => {
-                return pickedRestaurant 
-                    ? <PickedRestaurant restaurant={pickedRestaurant} />
-                    : <NotPicked />
-            }}
-        </RestaurantsContext.Consumer>
-    </div>
-)
+class Randomizer extends Component {
+    state = {
+        loading: false,
+    }
+
+    fakeLoading = () => {
+        this.setState({ loading: true });
+        setTimeout(() => {this.setState({ loading: false })}, 3000);
+    }
+
+    render() {
+        return (
+            <div className="randomizer">
+                <RestaurantsContext.Consumer>
+                    {({ pickedRestaurant }) => {
+                        return pickedRestaurant 
+                            ? <PickedRestaurant restaurant={pickedRestaurant} />
+                            : <NotPicked />
+                    }}
+                </RestaurantsContext.Consumer>
+            </div>
+            
+        );
+    }
+}
 
 export default Randomizer;
