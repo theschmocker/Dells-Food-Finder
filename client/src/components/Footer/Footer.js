a61be5b54c071d80f8416cdc466004f3dbe58857
theschmocker <jacobcs1996@gmail.com>
2018-07-04 22:04:27 -0500

    build out component

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import FaGithub from 'react-icons/lib/fa/github';
+import FaLinkedIn from 'react-icons/lib/fa/linkedin';
+import FaGlobe from 'react-icons/lib/fa/globe';
+
 const Footer = () => ( 
-    <footer
-        className="footer"
-    >
-        <h2>Dells Food Finder</h2>
-        <span>Built by <a href="https://jacobschmocker.com">Jacob Schmocker</a>.</span> <span>Copyright {new Date().getFullYear()}</span>
+    <footer className="footer">
+        <h2 className="footer__title">Dellish</h2>
+        <div className="footer__contact-container">
+            <h3 className="footer__contact-title">Contact / Suggestions:</h3>
+            <a href="mailto:jacobschmocker@gmail" className="footer__contact-email">jacobschmocker@gmail.com</a>
+        </div>
+        <div className="footer__social-container">
+            <p className="footer__social-madewith">Made with 🍕 by Jacob Schmocker</p> 
+            <div className="footer__social-icons">
+                <a 
+                    className="social-icon" 
+                    href="https://github.com/theschmocker"
+                ><FaGithub /></a>
+                <a 
+                    className="social-icon" 
+                    href=""
+                ><FaLinkedIn /></a>
+                <a 
+                    className="social-icon" 
+                    href=""
+                ><FaGlobe /></a>
+            </div>
+        </div>
+        <span className="footer__copyright">&copy; Copyright {new Date().getFullYear()}</span>
     </footer>
 )
 
